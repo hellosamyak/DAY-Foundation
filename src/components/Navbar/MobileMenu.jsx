4c37d823b6595d06b7614dfeb34612466481d986
hellosamyak <jainsamyak0805@gmail.com>
2025-05-22 23:23:26 +0530

    optimize 'Our Team' section for mobile

--- src/components/Navbar/MobileMenu.jsx
+++ src/components/Navbar/MobileMenu.jsx
@@ -7,6 +7,7 @@ export default function MobileMenu({ isOpen, onClose }) {
   const links = [
     { text: "Home", path: "/" },
     { text: "About Us", path: "/about" },
+    { text: "Our Team", path: "/team" },
     { text: "Our Work", path: "/work" },
     { text: "Get Involved", path: "/getinvolved" },
     { text: "Contact Us", path: "/contact" },
